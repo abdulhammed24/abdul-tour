df4c38e3b2beda5852dba7d127fd2adc65c068fa
flywave <flywavedigital@gmail.com>
2024-08-25 10:43:49 +0100

    changed to abdulTour on 25th August-2024

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ const bg = Bricolage_Grotesque({ subsets: ["latin"] });
 export const metadata = {
   title: `Explore Abdul Tour: Buy Your Tickets Now`,
   description: `Discover the exciting adventures with Abdul Tour! Secure your tickets today and embark on a memorable journey. Whether you're looking for unique travel experiences or unforgettable destinations, Abdul Tour offers curated packages to meet your travel needs. Don't miss out—purchase your tickets now and start your adventure with Abdul Tour!`,
-  metadataBase: new URL("https://afgm.netlify.app/"),
+  metadataBase: new URL("https://abtour.netlify.app/"),
 };
 
 export default function RootLayout({ children }) {
